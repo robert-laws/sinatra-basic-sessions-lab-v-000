27ffa6f743a8e808d5e255e92365a926b7397714
Robert Laws <roblaws@me.com>
2017-10-26 07:09:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ class App < Sinatra::Base
   end
 
   get "/" do
-
     erb :index
   end
 
   post "/checkout" do
     session["item"] = params[:item]
     @session = session
+    erb :checkout
   end
 end
